6cc0ed4d78db58c00ca480be95e2c87b2e82c2ae
hengjiaaan <90436717+hengjiaaan@users.noreply.github.com>
2023-12-14 23:24:18 -0800

    Update comps_main.m

--- comps_main.m
+++ comps_main.m
@@ -8,14 +8,24 @@
 stain = imread('ratstain.jpg');
 %imshow(stain);
 
-% Nissl stain
-purple = imread('purple_stain.png');
-%imshow(purple);
+% Pre-cropped AChEs Acetylcholinesterase histochemistry
+precrop_stain = imread("ratstain_lower_cropped.png");
+%imshow(precrop_stain)
+% type in the following if want to use the pre-cropped: stain = precrop_stain;
 
-% Atlas
+% Atlas, resize to dimensions to stain 
 ratlas = imread("ratlas.jpg");
 ratlas = imresize(ratlas, size(rgb2gray(stain)));
 %imshow(ratlas);
+
+precrop_ratlas = imread("atlas_lower_cropped.png");
+%imshow(precrop_stain)
+% type in the following if want to use the pre-cropped: ratlas = precrop_ratlas
+
+
+% Optional Timm-Nissl stain
+stain = imread('purple_stain.png');
+%imshow(stain);
 
 %% Cropping original image 
 % Focusing on specific parts of the stain & atlas
@@ -40,8 +50,8 @@ ratlas = cropped_atlas;
 
 %% Get matching points
 % n = number of points in one image
-% minimum 4 points, maximum 8 points
-n=4;
+% minimum 4 points, maximum ~8 points
+n=6;
 ratlas_pts = readPoints(ratlas, n);
 ratlas_pts = ratlas_pts';
 
@@ -50,7 +60,6 @@ stain_pts = readPoints(stain, n);
 stain_pts = stain_pts';
 
 %% Estimating and applying homography on atlas
-
 transformed_atlas = homography(ratlas, ratlas_pts, stain, stain_pts);
 
 
@@ -59,7 +68,7 @@ transformed_atlas = homography(ratlas, ratlas_pts, stain, stain_pts);
 % alpha level is any value 1 to 0
 % 1 being completely non transparent
 % 0 being completey transparent
-alpha_level = 0.5;
+alpha_level = 0.36;
 
 % non-filtered overlay
 overlay_atlas(transformed_atlas, stain, alpha_level)
@@ -68,6 +77,8 @@ overlay_atlas(transformed_atlas, stain, alpha_level)
 overlay_canny_atlas(transformed_atlas, stain, alpha_level)
 
 
+%overlay non-transformed atlas to compare results
+overlay_canny_atlas(ratlas, stain, alpha_level)
 
 
 
